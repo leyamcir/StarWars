7df198da9fed93c5560c1ad3463c76d83b743a6b
Alicia Daza <alicia@Alicias-Mac-mini.local>
2016-06-27 21:24:00 +0200

    JSONProcessing + types

---
+++ StarWars/JSONProcessing.swift
@@ -0,0 +1,14 @@
+//
+//  JSONProcessing.swift
+//  StarWars
+//
+//  Created by Alicia Daza on 27/06/16.
+//  Copyright © 2016 Alicia Daza. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Aliases
+typealias JSONObject = AnyObject
+typealias JSONDictionary = [String: JSONObject]
+typealias JSONArray = [JSONDictionary]
