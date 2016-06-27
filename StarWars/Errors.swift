13927c7add9ec63ee680a3edd9eab09c1489051c
Alicia Daza <alicia@Alicias-Mac-mini.local>
2016-06-27 21:10:33 +0200

    StarWarsError ErrorType

---
+++ StarWars/Errors.swift
@@ -0,0 +1,17 @@
+//
+//  Errors.swift
+//  StarWars
+//
+//  Created by Alicia Daza on 27/06/16.
+//  Copyright © 2016 Alicia Daza. All rights reserved.
+//
+
+import Foundation
+
+// MARK: JSON Errors
+enum StarWarsError : ErrorType {
+    case wrongURLFormatForJSONResource
+    case resourcePointedByUrLNotReachable
+    case jsonParsingError
+    case wrongJSONFormat    
+}
