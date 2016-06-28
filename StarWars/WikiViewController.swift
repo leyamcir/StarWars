70809bf6079723f9dada3adc565d02a4e0318f3f
Alicia Daza <amdazacastillo@gmail.com>
2016-06-28 23:16:35 +0200

    wikiVC without delegate

--- StarWars/WikiViewController.swift
+++ StarWars/WikiViewController.swift
@@ -10,12 +10,38 @@ import UIKit
 
 class WikiViewController: UIViewController {
 
+    // MARK: - Properties
+    var model : StarWarsCharacter
+    
+    @IBOutlet weak var browser: UIWebView!
+    
+    
+    // MARK: - Initialization
+    init(model: StarWarsCharacter) {
+        self.model = model
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - Syncing
+    func syncModelWithView() {
+        browser.loadRequest(NSURLRequest(URL: model.url))
+    }
+    
+    // MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
     }
 
+    // MARK: - Memory
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
