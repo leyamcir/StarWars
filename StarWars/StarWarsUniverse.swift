4ca2b3ef030567a0783676f1e3da52dcc9fc6cb3
Alicia Daza <amdazacastillo@gmail.com>
2016-06-29 00:30:56 +0200

    TODO functions for Table View Controller

--- StarWars/StarWarsUniverse.swift
+++ StarWars/StarWarsUniverse.swift
@@ -13,6 +13,35 @@ class StarWarsUniverse {
     //MARK: Utility types
     typealias StarWarsArray = [StarWarsCharacter]
     typealias StarWarsDictionary = [StarWarsAffiliation: StarWarsArray]
+    
+    
+    init(){
+        // TODO:
+        // Load two JSON data
+        
+        
+    }
+    
+    var affiliationCount : Int {
+        get {
+            // TODO: Get affiliations count
+            return 0
+        }
+    }
+    
+    func characterCount(forAffiliation affiliation: StarWarsAffiliation) -> Int {
+        // TODO:
+        // Get characters for this affiliation
+    }
+    
+    func character(atIndex index: Int, forAffiliation affiliation: StarWarsAffiliation) -> StarWarsCharacter {
+        //TODO
+        // Character at index position in affiliation
+    }
+    
+    func affiliationName(affiliation: StarWarsAffiliation) -> String {
+        
+    }
 
     // MARK: Utils
     func makeEmptyAffiliations() -> StarWarsDictionary {
