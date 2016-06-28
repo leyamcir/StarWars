e30b33614c43d44cccc8722ee35a990ee9f2d3fa
Alicia Daza <amdazacastillo@gmail.com>
2016-06-28 23:00:38 +0200

    Created WikiViewController

---
+++ StarWars/WikiViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WikiViewController.swift
+//  StarWars
+//
+//  Created by Alicia Daza on 28/06/16.
+//  Copyright © 2016 Alicia Daza. All rights reserved.
+//
+
+import UIKit
+
+class WikiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
